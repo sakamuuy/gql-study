e0930975b71f473d5ba001b00bcc34d9dd1e3058
sakamuuy <sakamuu.y@gmail.com>
2021-07-07 10:14:10 +0900

    Add queries and mutations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,24 @@
 import { ApolloServer, gql, IResolvers } from 'apollo-server';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { PrismaSelect } from '@paljs/plugins';
+import { title } from 'process';
 
 const typeDefs = gql`
   type Post {
-    id: ID
-    title: String 
+    id: ID!
+    title: String!
     content: String
-    published: Boolean
-    authorId: Int
+    published: Boolean!
+    authorId: Int!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  input AddPostInput {
+    title: String!
+    content: String
+    published: Boolean!
+    authorId: Int!
   }
 
   type Profile {
@@ -18,11 +28,30 @@ const typeDefs = gql`
   }
 
   type User {
-    id: ID
-    email: String
-    name: String
-    posts: [Post!]!
+    id: ID!
+    email: String!
+    name: String!
+    posts: [Post]
     profile: Profile
+  }
+
+  input AddUserInput {
+    email: String!
+    name: String!
+  }
+
+  type Query {
+    posts: [Post!]!
+    post(id: ID!): Post
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    addUser(addUserInput: AddUserInput!): User
+    addPost(addPostInput: AddPostInput!): Post
+    editPost(postId: ID!, addPostInput: AddPostInput): Post
+    deletePost(postId: ID!): User
   }
 `;
 
@@ -30,8 +59,149 @@ const resolvers: IResolvers<any, { readonly prismaClient: PrismaClient }> = {
   Query: {
     post(_root, { id }: { readonly id: string }, ctx, info) {
       const select = new PrismaSelect(info);
-    }
+      return ctx.prismaClient.post.findUnique({
+        ...select.value,
+        where: {
+          id
+        }
+      })
+    },
+    posts(_root, _argx, ctx, info) {
+      const select = new PrismaSelect(info);
+      return ctx.prismaClient.post.findMany({
+        ...select.value
+      });
+    },
+    user(_root, { id }: { readonly id: string }, ctx, info) {
+      const idn = Number(id);
+      const select = new PrismaSelect(info);
+      return ctx.prismaClient.user.findUnique({
+        ...select.value,
+        where: {
+          id: idn
+        }
+      })
+    },
+    users(_root, _argx, ctx, info) {
+      const select = new PrismaSelect(info);
+      return ctx.prismaClient.user.findMany({
+        ...select.value
+      });
+    },
   },
+  Mutation: {
+    async addUser(
+      _root,
+      {
+        addUserInput: {
+          email,
+          name
+        }
+      }: {
+        readonly addUserInput: {
+          readonly email: string;
+          readonly name: string;
+        }
+      },
+      ctx
+    ) {
+      const user = await ctx.prismaClient.user.findUnique({
+        where: {
+          email: email
+        }
+      });
+      if (user) return null;
+
+      const newUser = await ctx.prismaClient.user.create({
+        data: {
+          email,
+          name
+        }
+      });
+      return newUser;
+    },
+    async addPost(
+      _root,
+      {
+        addPostInput: {
+          title,
+          content,
+          published,
+          authorId
+        }
+      }: {
+        readonly addPostInput: {
+          readonly title: string;
+          readonly content?: string
+          readonly published: boolean
+          readonly authorId: number
+        };
+      },
+      ctx
+    ) {
+      const post = await ctx.prismaClient.post.create({
+        data: {
+          title,
+          content,
+          published,
+          authorId
+        }
+      });
+      return post;
+    },
+    async editPost(
+      _root,
+      {
+        postId,
+        addPostInput: {
+          title,
+          content,
+          published,
+          authorId
+        }
+      }: {
+        readonly postId: number;
+        readonly addPostInput: {
+          readonly title: string;
+          readonly content?: string
+          readonly published: boolean
+          readonly authorId: number
+        };
+      },
+      ctx
+    ) {
+      const post = await ctx.prismaClient.post.findUnique({
+        where: {
+          id: postId
+        }
+      });
+      if (!post) return;
+      await ctx.prismaClient.post.update({
+        where: {
+          id: postId,
+        },
+        data: {
+          ...post,
+          title,
+          content,
+          published,
+          authorId
+        }
+      });
+    },
+    async deletePost(
+      _root,
+      { postId }: { readonly postId: number },
+      ctx
+    ) {
+      await ctx.prismaClient.post.delete({
+        where: {
+          id: postId
+        }
+      });
+      return postId
+    }
+  }
 };
 
 class PrismaClientPool {
