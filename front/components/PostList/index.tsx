6b66736f47871855470a516167f47bc1b0582c2f
sakamuuy <sakamuu.y@gmail.com>
2021-07-08 23:42:37 +0900

    Add apollo client

--- front/components/PostList/index.tsx
+++ front/components/PostList/index.tsx
@@ -1,18 +1,36 @@
 import { VFC } from 'react';
 import ListItem from '../atoms/ListItem';
 import styles from './postList.module.css'
+import { gql, useQuery } from '@apollo/client';
+import { PostsQuery, PostsQueryVariables } from './__generated__/posts-query';
+
+const POSTS_QUERY = gql`
+  query PostsQuery {
+    posts {
+      id
+      title
+      content
+    }
+  }
+`;
 
 const PostList = () => {
+
+  const { loading, error, data } = useQuery<PostsQuery, PostsQueryVariables>(POSTS_QUERY);
+
+  if (loading || !data?.posts.length) return <></>;
+
+  if (error) return <div>'error'</div>;
+
   return (
     <ul className={styles.postList}>
-      {[0,1,2,3,4].map((i) => (
-        <li key={i} className={styles.postListItem}>
+      {data.posts?.map((post) => (
+        <li key={post.id} className={styles.postListItem}>
           <ListItem>
             <>
-              <h4 className={styles.postListItem__title}>title</h4>
-              <p className={styles.postListItem__body}>テキストテキストテキストテキストテキストテキストテキストテトテキストテキスト
-              テキストテキストテキストテキストテキストテキストテキストテトテキストテキスト
-              テキストテキストテキストテキストテキストテキストテキストテトテキストテキスト
+              <h4 className={styles.postListItem__title}>{post.title}</h4>
+              <p className={styles.postListItem__body}>
+                {post.content}
               </p>
             </>
           </ListItem>
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,11 +1,23 @@
 import Header from '../components/Header'
 import PostList from '../components/PostList';
+import { 
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider 
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+})
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <PostList />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Header />
+        <PostList />
+      </div>
+    </ApolloProvider>
   )
 }
